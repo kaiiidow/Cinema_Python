734e8606fa00483f85be79bdb5bc6f1c82fcbd6d
kaiiidow <adame.archi@gmail.com>
2025-11-21 21:08:38 +0100

    Delete cinema_python directory

--- cinema_python/exceptions/__init__.py
+++
@@ -1,4 +0,0 @@
-from .film_inexistant import FilmInexistantException
-from .salle_pleine import SallePleineException
-
-__all__ = ["FilmInexistantException", "SallePleineException"]
--- cinema_python/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .film import Film
-from .salle import Salle
-from .reservation import Reservation
-
-__all__ = ["Film", "Salle", "Reservation"]
