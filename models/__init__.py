ce4dfd9f019095c3c757b262fb9af0b299ecbcd6
kaiiidow <adame.archi@gmail.com>
2025-11-21 21:51:09 +0100

    Delete models directory

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .film import Film
-from .salle import Salle
-from .reservation import Reservation
-from .enums import TypeSalle, StyleFilm
-
-__all__ = ["Film", "Salle", "Reservation", "TypeSalle", "StyleFilm"]
